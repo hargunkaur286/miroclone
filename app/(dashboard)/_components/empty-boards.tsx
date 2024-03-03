eb21b8dda63f8ed7d6692ad8b3a7ea11365ef394
Hargun Kaur <hargunkaur286@gmail.com>
2024-03-03 22:18:07 +0530

    added mutation

--- app/(dashboard)/_components/empty-boards.tsx
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -1,7 +1,31 @@
+"use client"
+
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { useOrganization } from "@clerk/nextjs"; 
+
+import { api } from "@/convex/_generated/api";
+import { useApiMutation } from "@/hooks/use-api-mutation";
+import { toast } from "sonner";
 
 export const EmptyBoards = () => {
+
+    const {organization} = useOrganization();
+    const { mutate, pending } = useApiMutation(api.board.create);
+
+    const onClick = () => {
+        if(!organization) return;
+
+        mutate({
+            orgId: organization.id,
+            title: "Untitled"
+        })
+        .then((id) => {
+            toast.success("Board Created!");
+        })
+        .catch(() => toast.error("Failed to create board!"));
+    };
+
     return (
         <div className="h-full flex flex-col items-center justify-center">
             <Image 
@@ -17,7 +41,7 @@ export const EmptyBoards = () => {
                 Start by creating a board for your organization!
             </p>
             <div className="mt-6">
-                <Button size="lg">
+                <Button disabled={pending} onClick={onClick} size="lg">
                     Create Board Text
                 </Button>
             </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import { Toaster } from "@/components/ui/sonner";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -19,6 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ConvexClientProvider>
+          <Toaster />
           {children}
         </ConvexClientProvider>
         </body>
