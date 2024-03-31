a82cdeff68284c87e7b4ac9ae9f70e297caeaae8
Hargun Kaur <hargunkaur286@gmail.com>
2024-03-31 20:06:30 +0530

    search querying

--- app/(dashboard)/_components/new-board-button.tsx
+++ app/(dashboard)/_components/new-board-button.tsx
@@ -6,6 +6,7 @@ import { Plus } from "lucide-react";
 // import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { useApiMutation } from "@/hooks/use-api-mutation";
+import { useRouter } from "next/navigation";
 
 interface NewBoardButtonProps {
     orgId: string;
@@ -16,7 +17,7 @@ export const NewBoardButton = ({
     orgId,
     disabled,
 }: NewBoardButtonProps) => {
-
+    const router = useRouter();
     const { mutate, pending } = useApiMutation(api.board.create);
 
     const onClick = () => {
@@ -26,6 +27,7 @@ export const NewBoardButton = ({
         })
         .then((id) => {
             toast.success("Board Created");
+            router.push(`/board/${id}`);
         })
         .catch(() => toast.error("Failed to create board"));
     }
