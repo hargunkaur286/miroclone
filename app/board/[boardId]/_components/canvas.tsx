56b775ec713460aa2f8665fbed6afee52f17ee41
Hargun Kaur <hargunkaur286@gmail.com>
2024-03-31 23:10:57 +0530

    room authentication

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -3,6 +3,8 @@ import { Info } from "./info"
 import { Participants } from "./participants"
 import { Toolbar } from "./toolbar"
 
+import { useSelf } from "@/liveblocks.config";
+
 interface CanvasProps {
     boardId: string;
 };
@@ -10,6 +12,10 @@ interface CanvasProps {
 export const Canvas = ({
     boardId,
 }: CanvasProps) => {
+    const {info} = useSelf((me) => me.info);
+
+    console.log(info);
+    
     return (
         <main className="h-full w-full relative bg-neutral-100 touch-none">
             <Info />
--- liveblocks.config.ts
+++ liveblocks.config.ts
@@ -2,8 +2,8 @@ import { createClient } from "@liveblocks/client";
 import { createRoomContext, createLiveblocksContext } from "@liveblocks/react";
   
 const client = createClient({
-  publicApiKey: "pk_dev_HkYW0BjE0od0vwZTKMcvwS1pagc4p-jeEgnIL1xtMBLQxJc7XgpvtoZkKRljPqBN",
-  // authEndpoint: "/api/liveblocks-auth",
+  // publicApiKey: "pk_dev_HkYW0BjE0od0vwZTKMcvwS1pagc4p-jeEgnIL1xtMBLQxJc7XgpvtoZkKRljPqBN",
+  authEndpoint: "/api/liveblocks-auth",
   // throttle: 100,
   async resolveUsers({ userIds }) {
     // Used only for Comments and Notifications. Return a list of user information
@@ -68,8 +68,11 @@ type Storage = {
 // provided by your own custom auth back end (if used). Useful for data that
 // will not change during a session, like a user's name or avatar.
 type UserMeta = {
-  // id?: string,  // Accessible through `user.id`
-  // info?: Json,  // Accessible through `user.info`
+  id?: string;
+  info?: {
+    name?: string;
+    picture?: string;
+  };
 };
 
 // Optionally, the type of custom events broadcast and listened to in this
